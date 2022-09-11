f09140b5df8e02569cb0498ce48ec881d85e87b7
Mohamad <abdulbaki11@hotmail.com>
2022-09-11 15:11:25 +1000

    added write all ratings to csv

--- google-api/rating_scale_analysis.py
+++ google-api/rating_scale_analysis.py
@@ -4,11 +4,13 @@ from pprint import pprint
 from matplotlib import pyplot as plt
 import numpy as np
 import csv
+import pandas as pd
 
 
 def rating_scale():
     directory = 'data'
 
+    website_ratings_frequency = {}
     # iterate over files in that directory
     for filename in os.listdir(directory):
         file = os.path.join(directory, filename)
@@ -43,10 +45,10 @@ def rating_scale():
                         year_list = year_dict[year]
                         year_list.append(rating)
 
-        print(counter)
-        print(review_counter)
+        # print(counter)
+        # print(review_counter)
 
-        pprint(year_dict)
+        # pprint(year_dict)
 
         frequency_dict = {}
 
@@ -61,8 +63,8 @@ def rating_scale():
 
         # For any rating that has less than 5 frequency, remove it and add to 'other' rating
         for year, frequencies in frequency_dict.items():
-            print(year)
-            print(frequencies)
+            # print(year)
+            # print(frequencies)
             ratings_to_delete = []
             other = 0
             for rating, frequency in frequencies.items():
@@ -73,9 +75,12 @@ def rating_scale():
             for rating in ratings_to_delete:
                 del frequencies[rating]
 
-        pprint(frequency_dict)
+        # pprint(frequency_dict)
 
         # write_to_csv(filename, frequency_dict)
+        website_ratings_frequency[filename[:-5]] = frequency_dict
+
+    return website_ratings_frequency
 
 
 def plot_results(frequency_dict: dict):
@@ -103,8 +108,36 @@ def write_to_csv(filename, frequency_dict: dict):
         print("I/O error")
 
 
+# convert the structure, instead of having the year as key and frequency as value {2020: 150}, have a year key and frequency key {year: 2020, frequency: 150}
+def write_all_ratings_to_csv(website_ratings_frequency: dict):
+    csv_columns = ['Website', 'Year', 'Rating', 'Frequency']
+    csv_file = f"frequency-csv/rating_scales.csv"
+    pprint(website_ratings_frequency)
+
+    try:
+        with open(csv_file, 'w', newline='', encoding="utf-8") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            writer.writeheader()
+
+            for website, year_dict in website_ratings_frequency.items():
+                for year, freq_dict in year_dict.items():
+                    for rating, frequency in freq_dict.items():
+                        writer.writerow({'Website': website, 'Year': year, 'Rating': rating, 'Frequency': frequency})
+    except IOError:
+        print("I/O error")
+
+
+
+def convert_website_ratings_frequency_to_csv(website_ratings_frequency: dict):
+    print(website_ratings_frequency)
+    df = pd.json_normalize(website_ratings_frequency)
+    print(df)
+
+
 def main():
-    rating_scale()
+    website_ratings_frequency = rating_scale()
+    write_all_ratings_to_csv(website_ratings_frequency)
+    # convert_website_ratings_frequency_to_csv(website_ratings_frequency)
 
 
 if __name__ == "__main__":
