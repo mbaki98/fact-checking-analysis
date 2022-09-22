c64d2019748db42b5affccce791d129c6f2bc968
Nofil <mkha7677@uni.sydney.edu.au>
2022-09-23 09:31:24 +1000

    fixed claimant categroiser working on gettin %'s for each claimants ratings

--- google-api/org_bias_checker.py
+++ google-api/org_bias_checker.py
@@ -3,6 +3,7 @@ import json
 from collections import defaultdict, Counter
 from pprint import pprint
 
+
 def split_by_year_v2(data, year_dict, politicians: set):
     # for every claim in the file
     for claim in data['claims']:
@@ -15,32 +16,38 @@ def split_by_year_v2(data, year_dict, politicians: set):
         claimant = return_category(claimant)
 
         for review in claim['claimReview']:
+            print(review['textualRating'])
+            rating = review['textualRating']
             if 'reviewDate' in review.keys():
                 year = review['reviewDate'][0:4]
             else:
                 continue  # skip this one, move to next claimReview
 
-            year_dict[year][claimant] += 1
+            # create if statement to check if year is in year_dict
+            if year in year_dict.keys():
+                year_dict[year] = [claimant][claim['claimReview']
+                [0]['textualRating']]
 
     return year_dict
 
 
 # f = open('data/politifact.json', encoding='utf-8')
 def main():
-    directory = 'data'
+    #     directory = 'data'
     politicians = get_politician_list()
 
     f = open('data/testFile.json', encoding='utf-8')
     data = json.load(f)
 
-    year_dict = defaultdict(Counter)
+    year_dict = {}
     year_dict = split_by_year_v2(data, year_dict, politicians)
 
-    for year, (counter) in year_dict.items():
-        print(year)
-        for k, v in counter.items():
-            print(k, v)
-    # pprint(year_dict)
+    # for year, (counter) in year_dict.items():
+    #     print(year)
+    #     for k, v in counter.items():
+    #         print(k, v)
+    pprint(year_dict)
+
 
 if __name__ == "__main__":
     main()
--- google-api/util/nofils_claimant_categoriser.py
+++ google-api/util/nofils_claimant_categoriser.py
@@ -4,7 +4,7 @@ import csv
 def return_category(claim: str):
     claim = claim.lower()
     social_media = ['social media', 'facebook', 'twitter', 'instagram', 'tweet', 'fb', 'tweet', 'social', 'tiktok.com',
-                    'tiktok', 'i.imgur.com']
+                    'tiktok', 'i.imgur.com', 'social media users', 'Social media users', 'social media user', 'Social media user', 'post', 'public', 'video', 'image', '8shit.net']
     multiple_source = ['mutliple sources', 'multiple resources', 'multiples sources', 'various sources',
                        'sources multiples', 'multiple people', 'multiple source', 'multiple', 'several', 'مصادر عدّة',
                        'عدة مصادر', 'مصادر عدة', 'أطراف عدّة']
@@ -14,15 +14,16 @@ def return_category(claim: str):
              'donaldjtrump.com', 'team trump', 'the trump campaign', 'newsdonaldtrumps.com', 'president trumps lawyers',
              'truetrumpers.com']
     biden = ['joe biden', 'biden']
-    news_sites = ['cbsnews.us', 'realrawnews.com', 'duffelblog.com', 'southendnewsnetwork.net', 'your news wire', 'yournewswire.com',
-                  'huzlers.com', 'the times', 'boom', 'Dailyexpose.Uk', 'the expose', 'thegatewaypundit.com',
+    news_sites = ['cbsnews.us', 'world news daily report','babylonbee.com','thereisnews.com','al jazeera','realrawnews.com', 'duffelblog.com', 'southendnewsnetwork.net', 'your news wire', 'yournewswire.com',
+                  'huzlers.com', 'the times', 'boom', 'dailyexpose.Uk', 'the expose', 'thegatewaypundit.com',
                   'rumble.com', 'daily express', 'bitchute.com', 'real raw news', 'the telegraph', 'the guardian',
                   'beforeitsnews.com', 'redvoicemedia.com', 'daily mail', 'the sun', 'the gateway pundit', 'tvc news',
                   'the daily expose', 'daily expose', 'bestnewshere.com', 'sky news', 'the africa report',
                   'thetruereporter.com', 'the independent', 'daily telegraph', 'daily mail', 'infowars', 'infowars.com',
                   'dailyexpose.co.uk', 'bbc news', 'trend news', 'africanews', 'worldnewsdailyreport.com',
                   'worldgreynews.com', 'worldnewsdailyreport', 'world grey news', 'westernjournal.com', 'the mirror',
-                  'the herald', 'the daily telegraph', 'skyline news', 'sky news kenya', 'naturalnews.com', 'daily mirror', 'theexpose.uk', 'natural news', 'mail online']
+                  'the herald', 'the daily telegraph', 'skyline news', 'sky news kenya', 'naturalnews.com', 'daily mirror', 'theexpose.uk', 'natural news', 'mail online', 'gb news', 'zee news', 'bbc', 'a nigerian newspaper', 'cape talk']
+    viral_post = ['viral image', 'viral video', 'viral post', 'viral rumor', 'viral quote', 'viral photo', 'viral footage', 'viral images', 'viral story', 'viral meme']
     pol_info = get_politician_list()
     # returns true if claim contains any elements from list social media
     if [element for element in social_media if (element in claim)]:
@@ -33,20 +34,31 @@ def return_category(claim: str):
         return 'youtube'
     if [element for element in whatsapp if (element in claim)]:
         return 'whatsapp'
-    if [element for element in trump if (element in claim)]:
-        return 'donald trump'
-    if [element for element in biden if (element in claim)]:
-        return 'joe biden'
-    # if [element for element in pol_info.keys() if (element in claim)]:
-    #     return 'US politicians - '
+    # if [element for element in trump if (element in claim)]:
+    #     return 'donald trump'
+    # if [element for element in biden if (element in claim)]:
+    #     return 'joe biden'
+    if [element for element in pol_info.keys() if (element in claim)] or 'labour' or 'labor' or 'Labour' or 'Conservative' or 'conservative' or 'Democratic' or 'democratic' in claim:
+        for key, value in pol_info.items():
+            if key in claim:
+                # print('key: '+key)
+                # print('claim: '+claim)
+                if "republican" in value.lower():
+                    return 'Republicans'
+                else:
+                    return 'Democrats'
+        # return 'US politicians - '
     if [element for element in news_sites if (element in claim)]:
-        return 'news site'
+        return 'news sites'
 
-    for key, value in pol_info.items():
-        if key in claim:
-            # print('key: '+key)
-            # print('claim: '+claim)
-            return 'US politician - ' + value
+    # for key, value in pol_info.items():
+    #     if key in claim:
+    #         # print('key: '+key)
+    #         # print('claim: '+claim)
+    #         if "republican" in value.lower():
+    #             return 'Republicans'
+    #         else:
+    #             return 'Democrats'
 
     return claim
 
