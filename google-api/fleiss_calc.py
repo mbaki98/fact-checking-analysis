671914dfaced6fcfac394e275e8618d43b9b2f66
lifonnnn <n.k4@outlook.com>
2022-09-30 14:40:22 +1000

    fixed fleiss calc to take into account fact checks with 3 publishers

--- google-api/fleiss_calc.py
+++ google-api/fleiss_calc.py
@@ -26,9 +26,9 @@ Notes: claimReview is an array of 2 objects
 
 
 def main():
-    directory = 'test_folder'
+    directory = 'fleiss_pair_files'
 
-    f = open('multiple_test.json', encoding='utf-8')
+    f = open('multiple.json', encoding='utf-8')
     data = json.load(f)
     counter = 0
     for claim in data:
@@ -43,17 +43,63 @@ def main():
             if publisherOne == publisherTwo:
                 continue
 
-            filename = publisherOne + publisherTwo
-            file = os.path.join(directory, filename)
-            if os.path.isfile(file):
-                with open(file, 'a', encoding='UTF8', newline='') as f:
-                    writer = csv.writer(f)
-                    writer.writerow([textualRatingOne, textualRatingTwo])
+            print('number of publishers: ' + str(len(claimReview)))
+            # get the number of publishers
+            nPublishers = len(claimReview)
+
+            # if 3 then go thru pairs of files
+            if nPublishers == 3:
+                publisherThree = claimReview[2]['publisher']['name']
+                textualRatingThree = claimReview[2]['textualRating']
+                comboOne = publisherOne + publisherTwo
+                comboTwo = publisherOne + publisherThree
+                comboThree = publisherTwo + publisherThree
+
+                fileOne = os.path.join(directory, comboOne)
+                fileTwo = os.path.join(directory, comboTwo)
+                fileThree = os.path.join(directory, comboThree)
+
+                if os.path.isfile(fileOne):
+                    with open(fileOne, 'a', encoding='UTF8', newline='') as f:
+                        writer = csv.writer(f)
+                        writer.writerow([textualRatingOne, textualRatingTwo])
+                else:
+                    with open(directory + '/' + comboOne, 'w', encoding='UTF8', newline='') as f:
+                        writer = csv.writer(f)
+                        writer.writerow([publisherOne, publisherTwo])
+                        writer.writerow([textualRatingOne, textualRatingTwo])
+
+                if os.path.isfile(fileTwo):
+                    with open(fileTwo, 'a', encoding='UTF8', newline='') as f:
+                        writer = csv.writer(f)
+                        writer.writerow([textualRatingOne, textualRatingThree])
+                else:
+                    with open(directory + '/' + comboTwo, 'w', encoding='UTF8', newline='') as f:
+                        writer = csv.writer(f)
+                        writer.writerow([publisherOne, publisherThree])
+                        writer.writerow([textualRatingOne, textualRatingThree])
+
+                if os.path.isfile(fileThree):
+                    with open(fileThree, 'a', encoding='UTF8', newline='') as f:
+                        writer = csv.writer(f)
+                        writer.writerow([textualRatingTwo, textualRatingThree])
+                else:
+                    with open(directory + '/' + comboThree, 'w', encoding='UTF8', newline='') as f:
+                        writer = csv.writer(f)
+                        writer.writerow([publisherTwo, publisherThree])
+                        writer.writerow([textualRatingTwo, textualRatingThree])
             else:
-                with open(directory + '/' + filename, 'w', encoding='UTF8', newline='') as f:
-                    writer = csv.writer(f)
-                    writer.writerow([publisherOne, publisherTwo])
-                    writer.writerow([textualRatingOne, textualRatingTwo])
+                filename = publisherOne + publisherTwo
+                file = os.path.join(directory, filename)
+                if os.path.isfile(file):
+                    with open(file, 'a', encoding='UTF8', newline='') as f:
+                        writer = csv.writer(f)
+                        writer.writerow([textualRatingOne, textualRatingTwo])
+                else:
+                    with open(directory + '/' + filename, 'w', encoding='UTF8', newline='') as f:
+                        writer = csv.writer(f)
+                        writer.writerow([publisherOne, publisherTwo])
+                        writer.writerow([textualRatingOne, textualRatingTwo])
 
             # print(publisherOne)
             # print(publisherTwo)
@@ -62,6 +108,7 @@ def main():
         #     quit()
         # pprint(claim)
     print(counter)
+
 
 if __name__ == "__main__":
     main()
